d9d744d72376c5fee238ddde6adcd8ffbcfe46f9
Daniel Parker <danpker@gmail.com>
2018-10-07 17:04:54 +0100

    Update to send a struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
+use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
-use std::thread;
+use std::{thread, time};
 
+struct Message {
+    id: u64,
+}
 
 fn main() {
-    let (sender, receiver) = mpsc::channel();
+    let (sender, receiver): (Sender<Message>, Receiver<Message>) = mpsc::channel();
 
-    thread::spawn(move || {
-        sender.send("hello").unwrap();
-    });
+    thread::spawn(move || producer(sender));
+    consumer(receiver);
+}
 
-    println!("{}", receiver.recv().unwrap());
+fn producer(sender: Sender<Message>) {
+    let mut counter: u64 = 0;
+    loop {
+        let message = Message{id: counter};
+        sender.send(message).unwrap();
+        counter = counter + 1;
+    }
+}
+
+fn consumer(receiver: Receiver<Message>) {
+    loop {
+        match receiver.try_iter().next() {
+            Some(item) => println!("{}", item.id),
+            // Sleep for 10ms if there's nothing to save CPU
+            None => thread::sleep(time::Duration::from_millis(10)),
+        }
+    }
 }
